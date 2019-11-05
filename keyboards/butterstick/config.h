d0de70072f12069f0e68c90ff0e8c87574e11dd1
Kent <kentsd16@student.uia.no>
2019-11-05 23:25:11 +0100

    Personalizes default keymap

--- keyboards/butterstick/config.h
+++ keyboards/butterstick/config.h
@@ -9,7 +9,7 @@
 #define MANUFACTURER    g Heavy Industries
 #define PRODUCT         Butter Stick
 #define DESCRIPTION     Its a stick of butter
-#define VERSION 		"Paula Deen"
+#define VERSION 		"Hjertego "
 
 #define DEBOUNCE 5
 #define FORCE_NKRO
@@ -18,7 +18,7 @@
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 10
 #define MATRIX_ROW_PINS { F4, F5 }
-#define MATRIX_COL_PINS { B0, B1, B2, B3, B4, B5, B6, B7, C6, C7}
+#define MATRIX_COL_PINS { B0, B1, B2, B3, B4, B5, B6, B7, C6, C7 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
